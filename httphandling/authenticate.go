f9b0d8f2bd5540049580d23f818c3df27ee8f81a
Jonathan Turner <jt@jtnet.co.uk>
2018-11-30 18:02:46 +0000

    lint

--- httphandling/authenticate.go
+++ httphandling/authenticate.go
@@ -121,7 +121,8 @@ func krbValidate(c *config.Config, creds identity.Credentials, event eventLog) i
 		validationErrEvent(c, &event, err)
 		return id
 	}
-	tgsReq, tgsRep, err := cl.TGSREQ(tgsReq, k.CRealm, k.Ticket, k.DecryptedEncPart.Key, 0)
+	var tgsRep messages.TGSRep
+	tgsReq, tgsRep, err = cl.TGSREQ(tgsReq, k.CRealm, k.Ticket, k.DecryptedEncPart.Key, 0)
 	if err != nil {
 		err = fmt.Errorf("getting identity info failed - service ticket error: %v", err)
 		validationErrEvent(c, &event, err)
